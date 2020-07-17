9da8d4edc3b8e1bba5ac47eac47af6f046a33a1f
Gary Qiu <GaryQiu@aopen.com>
2020-07-17 15:55:25 +0800

    Enhance to print out the debug log.
    Command::
    C:\AOpen\Github\UEFI\edk2\Build\OvmfX64\DEBUG_VS2019\FV>"c:\Program Files\qemu\qemu-system-x86_64.exe" -pflash OVMF.fd -net none -serial file:serial.log -hda fat:rw:hda-contents

--- MdeModulePkg/Application/TestApp/TestApp.c
+++ MdeModulePkg/Application/TestApp/TestApp.c
@@ -1,11 +1,13 @@
 #include <Library/UefiApplicationEntryPoint.h> 
 #include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
 
 EFI_STATUS
 EFIAPI
 UefiMain
 (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE* SystemTable)
 {
+	DEBUG ((DEBUG_ERROR, "TestApp Entry\n" ));
 	SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello World\r\n");  // Body
 	return EFI_SUCCESS;
 }
